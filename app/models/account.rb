e9397a67b751654b5984676b22dc07e6fca43d24
Jon Botelho <jon@jbotelho.com>
2013-10-04 22:56:52 -0400

    Beef up import support on Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,25 +1,61 @@
+require 'csv'
+
 class Account < ActiveRecord::Base
   belongs_to :institution
   has_many :transactions
 
 
-  def self.import_ofx(path)
+  def self.import_csv(path)
+    headers = nil
+    transactions = []
+    transactions_by_id = {}
+    CSV.foreach(path) do |line|
+      # First iteration only
+      if headers.nil?
+        headers = line.map { |x| x.strip }
+        next
+      end
+      # Map the CSV row into a hash
+      trx = {related: []}
+      line.each_with_index do |val,i|
+        key = headers[i]
+        next if key.blank?
+        trx[key] = val
+      end
+      # Store and index it
+      transactions << trx
+      transactions_by_id[trx['Transaction ID']] = trx
+    end
+
+    # Store transaction relations
+    transactions.each do |trx|
+      ref = trx['Reference Txn ID']
+      if ref.present? and transactions_by_id[ref]
+        transactions_by_id[ref][:related] << trx
+      end
+    end
+
+    [transactions, transactions_by_id]
+  end
+
+  def import_ofx(path)
     ofx = OFX(path)
 
-    account_number = ofx.account.id
-    account_number = ofx.account.bank_id
+    #account_number = ofx.account.id
+    #account_number = ofx.account.bank_id
 
     ofx.account.transactions.each do |t|
       transactions.create!(
         amount: t.amount,
-        currency: t.currency,
-        date: t.date,
-        ofx_transaction: fit_id,
-        payee: payee,
-        type: type,
+        currency: ofx.account.currency,
+        date: t.posted_at,
+        ofx_transaction: t.fit_id,
+        payee: t.name,
+        type: t.type,
+        account_id: self.id,
       )
     end
-    balance = ofx.account.balance
+    self.balance = ofx.account.balance
     save!
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   belongs_to :parent, :class_name => Category
   has_many :rules
+  has_many :transactions
 end
